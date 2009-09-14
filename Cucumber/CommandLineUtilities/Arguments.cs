ca332bffa8bfc137e781d07361e07ba6a64528df
Chris Kooken <chris.kooken@gmail.com>
2009-09-14 14:54:33 -0400

    fixed command line parser

--- Cucumber/CommandLineUtilities/Arguments.cs
+++ Cucumber/CommandLineUtilities/Arguments.cs
@@ -4,212 +4,215 @@
  using System.Text;
  using System.Text.RegularExpressions;
 
-public class Arguments
+namespace Cucumber.CommandLineUtilities
 {
-    /// <summary>
-    /// Splits the command line. When main(string[] args) is used escaped quotes (ie a path “c:\folder\”)
-    /// Will consume all the following command line arguments as the one argument.
-    /// This function ignores escaped quotes making handling paths much easier.
-    /// http://jake.ginnivan.net/2009/07/c-argument-parser/
-    /// </summary>
-    /// <param name=”commandLine”>The command line.</param>
-    /// <returns></returns>
-    public static string[] SplitCommandLine(string commandLine)
+    public class Arguments
     {
-        var translatedArguments = new StringBuilder(commandLine);
-        var escaped = false;
-        for (var i = 0; i < translatedArguments.Length; i++)
+        /// <summary>
+        /// Splits the command line. When main(string[] args) is used escaped quotes (ie a path “c:\folder\”)
+        /// Will consume all the following command line arguments as the one argument.
+        /// This function ignores escaped quotes making handling paths much easier.
+        /// http://jake.ginnivan.net/2009/07/c-argument-parser/
+        /// </summary>
+        /// <param name=”commandLine”>The command line.</param>
+        /// <returns></returns>
+        public static string[] SplitCommandLine(string commandLine)
         {
-            if (translatedArguments[i] == '"')
+            var translatedArguments = new StringBuilder(commandLine);
+            var escaped = false;
+            for (var i = 0; i < translatedArguments.Length; i++)
             {
-                escaped = !escaped;
+                if (translatedArguments[i] == '"')
+                {
+                    escaped = !escaped;
+                }
+                if (translatedArguments[i] == ' ' && !escaped)
+                {
+                    translatedArguments[i] = '\n';
+                }
             }
-            if (translatedArguments[i] == ' ' && !escaped)
+
+            var toReturn = translatedArguments.ToString().Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            for (var i = 0; i < toReturn.Length; i++)
             {
-                translatedArguments[i] = '\n';
+                toReturn[i] = RemoveMatchingQuotes(toReturn[i]);
             }
+            return toReturn;
         }
 
-        var toReturn = translatedArguments.ToString().Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
-        for (var i = 0; i < toReturn.Length; i++)
+        public static string RemoveMatchingQuotes(string stringToTrim)
         {
-            toReturn[i] = RemoveMatchingQuotes(toReturn[i]);
-        }
-        return toReturn;
-    }
-
-    public static string RemoveMatchingQuotes(string stringToTrim)
-    {
-        var firstQuoteIndex = stringToTrim.IndexOf('"');
-        var lastQuoteIndex = stringToTrim.LastIndexOf('"');
-        while (firstQuoteIndex != lastQuoteIndex)
-        {
-            stringToTrim = stringToTrim.Remove(firstQuoteIndex, 1);
-            stringToTrim = stringToTrim.Remove(lastQuoteIndex - 1, 1); //-1 because we’ve shifted the indicies left by one
-            firstQuoteIndex = stringToTrim.IndexOf('"');
-            lastQuoteIndex = stringToTrim.LastIndexOf('"');
-        }
-
-        return stringToTrim;
-    }
-
-    private readonly Dictionary<string, Collection<string>> _parameters;
-    private string _waitingParameter;
-
-    public Arguments(IEnumerable<string> arguments)
-    {
-        _parameters = new Dictionary<string, Collection<string>>();
-
-        string[] parts;
-        
-        //Splits on beginning of arguments ( – and — and / )
-        //And on assignment operators ( = and : )
-        var argumentSplitter = new Regex(@"^-{1,2}|^/|=|:",
-            RegexOptions.IgnoreCase | RegexOptions.Compiled);
-
-        foreach (var argument in arguments)
-        {
-            parts = argumentSplitter.Split(argument, 3);
-            switch (parts.Length)
+            var firstQuoteIndex = stringToTrim.IndexOf('"');
+            var lastQuoteIndex = stringToTrim.LastIndexOf('"');
+            while (firstQuoteIndex != lastQuoteIndex)
             {
-                case 1:
-                    AddValueToWaitingArgument(parts[0]);
-                    break;
-                case 2:
-                    AddWaitingArgumentAsFlag();
+                stringToTrim = stringToTrim.Remove(firstQuoteIndex, 1);
+                stringToTrim = stringToTrim.Remove(lastQuoteIndex - 1, 1); //-1 because we’ve shifted the indicies left by one
+                firstQuoteIndex = stringToTrim.IndexOf('"');
+                lastQuoteIndex = stringToTrim.LastIndexOf('"');
+            }
 
-                    //Because of the split index 0 will be a empty string
-                    _waitingParameter = parts[1];
-                    break;
-                case 3:
-                    AddWaitingArgumentAsFlag();
+            return stringToTrim;
+        }
 
-                    //Because of the split index 0 will be a empty string
-                    string valuesWithoutQuotes = RemoveMatchingQuotes(parts[2]);
+        private readonly Dictionary<string, Collection<string>> _parameters;
+        private string _waitingParameter;
 
-                    AddListValues(parts[1], valuesWithoutQuotes.Split(','));
-                    break;
+        public Arguments(IEnumerable<string> arguments)
+        {
+            _parameters = new Dictionary<string, Collection<string>>();
+
+            string[] parts;
+        
+            //Splits on beginning of arguments ( – and — and / )
+            //And on assignment operators ( = and : )
+            var argumentSplitter = new Regex(@"^-{1,2}|^/|=|:",
+                                             RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+            foreach (var argument in arguments)
+            {
+                parts = argumentSplitter.Split(argument, 3);
+                switch (parts.Length)
+                {
+                    case 1:
+                        AddValueToWaitingArgument(parts[0]);
+                        break;
+                    case 2:
+                        AddWaitingArgumentAsFlag();
+
+                        //Because of the split index 0 will be a empty string
+                        _waitingParameter = parts[1];
+                        break;
+                    case 3:
+                        AddWaitingArgumentAsFlag();
+
+                        //Because of the split index 0 will be a empty string
+                        string valuesWithoutQuotes = RemoveMatchingQuotes(parts[2]);
+
+                        AddListValues(parts[1], valuesWithoutQuotes.Split(','));
+                        break;
+                }
+            }
+
+            AddWaitingArgumentAsFlag();
+        }
+
+        private void AddListValues(string argument, IEnumerable<string> values)
+        {
+            foreach (var listValue in values)
+            {
+                Add(argument, listValue);
             }
         }
 
-        AddWaitingArgumentAsFlag();
-    }
-
-    private void AddListValues(string argument, IEnumerable<string> values)
-    {
-        foreach (var listValue in values)
+        private void AddWaitingArgumentAsFlag()
         {
-            Add(argument, listValue);
+            if (_waitingParameter == null) return;
+
+            AddSingle(_waitingParameter, "true");
+            _waitingParameter = null;
         }
-    }
 
-    private void AddWaitingArgumentAsFlag()
-    {
-        if (_waitingParameter == null) return;
-
-        AddSingle(_waitingParameter, "true");
-        _waitingParameter = null;
-    }
-
-    private void AddValueToWaitingArgument(string value)
-    {
-        if (_waitingParameter == null) return;
-
-        value = RemoveMatchingQuotes(value);
-
-        Add(_waitingParameter, value);
-        _waitingParameter = null;
-    }
-
-    /// <summary>
-    /// Gets the count.
-    /// </summary>
-    /// <value>The count.</value>
-    public int Count
-    {
-        get
+        private void AddValueToWaitingArgument(string value)
         {
-            return _parameters.Count;
+            if (_waitingParameter == null) return;
+
+            value = RemoveMatchingQuotes(value);
+
+            Add(_waitingParameter, value);
+            _waitingParameter = null;
         }
-    }
 
-    /// <summary>
-    /// Adds the specified argument.
-    /// </summary>
-    /// <param name=”argument”>The argument.</param>
-    /// <param name=”value”>The value.</param>
-    public void Add(string argument, string value)
-    {
-        if (!_parameters.ContainsKey(argument))
-            _parameters.Add(argument, new Collection<string>());
-
-        _parameters[argument].Add(value);
-    }
-
-    public void AddSingle(string argument, string value)
-    {
-        if (!_parameters.ContainsKey(argument))
-            _parameters.Add(argument, new Collection<string>());
-        else
-            throw new ArgumentException(string.Format("Argument {0} has already been defined", argument));
-
-        _parameters[argument].Add(value);
-    }
-
-    public void Remove(string argument)
-    {
-        if (_parameters.ContainsKey(argument))
-            _parameters.Remove(argument);
-    }
-
-    /// <summary>
-    /// Determines whether the specified argument is true.
-    /// </summary>
-    /// <param name=”argument”>The argument.</param>
-    /// <returns>
-    ///     <c>true</c> if the specified argument is true; otherwise, <c>false</c>.
-    /// </returns>
-    public bool IsTrue(string argument)
-    {
-        AssertSingle(argument);
-
-        var arg = this[argument];
-        
-        return arg != null && arg[0].Equals("true", StringComparison.OrdinalIgnoreCase);
-    }
-
-    private void AssertSingle(string argument)
-    {
-        if (this[argument] != null && this[argument].Count > 1)
-            throw new ArgumentException(string.Format("{0} has been specified more than once, expecting single value", argument));
-    }
-
-    public string Single(string argument)
-    {
-        AssertSingle(argument);
-
-        //only return value if its NOT true, there is only a single item for that argument
-        //and the argument is defined
-        if (this[argument] != null && !IsTrue(argument))
-            return this[argument][0];
-        
-        return null;
-    }
-
-    public bool Exists(string argument)
-    {
-        return (this[argument] != null && this[argument].Count > 0);
-    }
-
-    /// <summary>
-    /// Gets the <see cref=”System.Collections.ObjectModel.Collection&lt;T&gt;”/> with the specified parameter.
-    /// </summary>
-    /// <value></value>
-    public Collection<string> this[string parameter]
-    {
-        get
+        /// <summary>
+        /// Gets the count.
+        /// </summary>
+        /// <value>The count.</value>
+        public int Count
         {
-            return _parameters.ContainsKey(parameter) ? _parameters[parameter] : null;
+            get
+            {
+                return _parameters.Count;
+            }
+        }
+
+        /// <summary>
+        /// Adds the specified argument.
+        /// </summary>
+        /// <param name=”argument”>The argument.</param>
+        /// <param name=”value”>The value.</param>
+        public void Add(string argument, string value)
+        {
+            if (!_parameters.ContainsKey(argument))
+                _parameters.Add(argument, new Collection<string>());
+
+            _parameters[argument].Add(value);
+        }
+
+        public void AddSingle(string argument, string value)
+        {
+            if (!_parameters.ContainsKey(argument))
+                _parameters.Add(argument, new Collection<string>());
+            else
+                throw new ArgumentException(string.Format("Argument {0} has already been defined", argument));
+
+            _parameters[argument].Add(value);
+        }
+
+        public void Remove(string argument)
+        {
+            if (_parameters.ContainsKey(argument))
+                _parameters.Remove(argument);
+        }
+
+        /// <summary>
+        /// Determines whether the specified argument is true.
+        /// </summary>
+        /// <param name=”argument”>The argument.</param>
+        /// <returns>
+        ///     <c>true</c> if the specified argument is true; otherwise, <c>false</c>.
+        /// </returns>
+        public bool IsTrue(string argument)
+        {
+            AssertSingle(argument);
+
+            var arg = this[argument];
+        
+            return arg != null && arg[0].Equals("true", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private void AssertSingle(string argument)
+        {
+            if (this[argument] != null && this[argument].Count > 1)
+                throw new ArgumentException(string.Format("{0} has been specified more than once, expecting single value", argument));
+        }
+
+        public string Single(string argument)
+        {
+            AssertSingle(argument);
+
+            //only return value if its NOT true, there is only a single item for that argument
+            //and the argument is defined
+            if (this[argument] != null && !IsTrue(argument))
+                return this[argument][0];
+        
+            return null;
+        }
+
+        public bool Exists(string argument)
+        {
+            return (this[argument] != null && this[argument].Count > 0);
+        }
+
+        /// <summary>
+        /// Gets the <see cref=”System.Collections.ObjectModel.Collection&lt;T&gt;”/> with the specified parameter.
+        /// </summary>
+        /// <value></value>
+        public Collection<string> this[string parameter]
+        {
+            get
+            {
+                return _parameters.ContainsKey(parameter) ? _parameters[parameter] : null;
+            }
         }
     }
 }
