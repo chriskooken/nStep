ed1976fca94832a41702aed51627f9e572fe897e
Brendan Erwin <brendanjerwin@gmail.com>
2009-09-16 16:08:59 -0400

    Sexy syntax

---
+++ nucumber.core/ActionCaller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Nucumber.Core
+{
+    public class ActionCaller
+    {
+        private readonly Delegate action;
+        private readonly object[] parms;
+
+        public ActionCaller(Delegate action)
+        {
+            this.action = action;
+        }
+
+        public ActionCaller(Delegate action, params Object[] parms) : this(action)
+        {
+            this.parms = parms;
+        }
+
+        public void Call()
+        {
+            if (parms != null)
+            {
+                action.DynamicInvoke(parms);
+                return;
+            }
+            action.DynamicInvoke();
+        }
+    }
+}
