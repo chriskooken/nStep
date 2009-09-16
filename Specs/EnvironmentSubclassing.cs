279f0806fdc1bf80735e89a8d11a6a8555bdd4bb
Chris Kooken <chris.kooken@gmail.com>
2009-09-16 17:20:20 -0400

    did some command line stuff

---
+++ Specs/EnvironmentSubclassing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Specs
+{
+    [TestFixture]
+	public class EnvironmentSubclassing
+	{
+        [Test]
+        public void it_should_only_allow_you_to_subclass_EnvironmentBase_once_per_session()
+        {
+            throw new NotImplementedException();
+        }
+
+	}
+}
