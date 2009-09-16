bc503bc7d94d7de32fac39745b170b1a0d473bdb
Chris Kooken <chris.kooken@gmail.com>
2009-09-15 20:24:09 -0400

    refactored a little and added some specs

---
+++ Specs/CommandLine/FeatureFiles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.ExtensionsImpl;
+
+namespace CommandLine
+{
+    [TestFixture]
+    public class if_no_feature_files_are_specified 
+    {
+        [Test]
+        public void Then_it_should_load_all_features_in_the_given_directory()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestFixture]
+    public class A_feature_file_is_specified
+    {
+        [Test]
+        public void Then_it_should_load_a_single_feature()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
