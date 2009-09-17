1327e9f246141d0a6b15c8ebe90b31a2879bc146
Chris Kooken <chris.kooken@gmail.com>
2009-09-16 22:32:58 -0400

    modified typecaster to handle more types

--- Specs/TypeCaster.cs
+++ Specs/TypeCaster.cs
@@ -22,11 +22,48 @@ namespace Specs
         {
             AssertItWorks<string>("foobar");
         }
-        
+
+        [Test]
+        public void it_should_convert_things_to_Int64()
+        {
+            AssertItWorks<Int64>("75");
+        }
         [Test]
         public void it_should_convert_things_to_Int32()
         {
             AssertItWorks<Int32>("75");
+        }
+        [Test]
+        public void it_should_convert_things_to_double()
+        {
+            AssertItWorks<double>("75.54");
+        }
+        [Test]
+        public void it_should_convert_things_to_decimal()
+        {
+            AssertItWorks<decimal>("75.0124");
+        }
+        [Test]
+        public void it_should_convert_things_to_float()
+        {
+            AssertItWorks<float>(".11075");
+        }
+        [Test]
+        public void it_should_convert_things_to_DateTime()
+        {
+            AssertItWorks<DateTime>("5/11/2009");
+        }   
+        
+        [Test]
+        public void it_should_convert_things_to_Bool()
+        {
+            AssertItWorks<bool>("true");
+        } 
+        
+        [Test]
+        public void it_should_convert_things_to_Guid()
+        {
+            AssertItWorks<Guid>("83f32761-4794-470e-9d88-161a130488d4");
         }
 
 
--- nucumber.core/TypeCaster.cs
+++ nucumber.core/TypeCaster.cs
@@ -6,10 +6,32 @@ namespace Nucumber.Core
     {
         public object MakeIntoType(string value, Type type)
         {
-            if (type == typeof(string))
+            if (type == typeof (string))
                 return value;
-            if (type == typeof(Int32))
+
+            if (type == typeof (Int32))
                 return Int32.Parse(value);
+
+            if (type == typeof (Int64))
+                return Int64.Parse(value);
+
+            if (type == typeof (double))
+                return double.Parse(value);
+
+            if (type == typeof (decimal))
+                return decimal.Parse(value);
+
+            if (type == typeof (float))
+                return float.Parse(value);
+
+            if (type == typeof (DateTime))
+                return DateTime.Parse(value);
+
+            if (type == typeof (bool))
+                return bool.Parse(value);
+
+            if (type == typeof(Guid))
+                return new Guid(value);
 
             throw new InvalidCastException("Don't know how to convert: " + value + " into a " + type.Name);
         }
