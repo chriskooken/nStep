2ee4f951bee6e9cd467a4741162ac54248e6a906
adamrmoss <adamrmoss@yahoo.com>
2009-09-18 14:15:34 -0400

    Little refactoring.

--- nucumber.core/Parsers/GherkinParser.cs
+++ nucumber.core/Parsers/GherkinParser.cs
@@ -23,16 +23,29 @@ namespace Nucumber.Core.Parsers {
 
 		private static void CreateParsers()
 		{
+			CreateSimpleParsers();
+			CreateStepParsers();
+			CreateFeatureParsers();
+		}
+
+		private static void CreateSimpleParsers()
+		{
 			WhitespaceParser = Ops.Klenee(Prims.WhiteSpace);
 			CommentParser = Ops.Seq(WhitespaceParser, Ops.Seq('#', Prims.Rol));
+		}
 
+		private static void CreateStepParsers()
+		{
 			var givenParser = Ops.Seq(Ops.Seq(WhitespaceParser, Prims.Str("Given") | Prims.Str("given:")), Prims.Rol);
 			var whenParser = Ops.Seq(Ops.Seq(WhitespaceParser, Prims.Str("When") | Prims.Str("when:")), Prims.Rol);
 			var thenParser = Ops.Seq(Ops.Seq(WhitespaceParser, Prims.Str("Then") | Prims.Str("then:")), Prims.Rol);
 			var andParser = Ops.Seq(Ops.Seq(WhitespaceParser, Prims.Str("And") | Prims.Str("and:") | Prims.Str("But") | Prims.Str("but:")), Prims.Rol);
 			StepParser = CommentParser | givenParser | whenParser | thenParser | andParser;
+		}
 
-			var backgroundHeaderParser = Ops.Seq(WhitespaceParser, Ops.Seq(Prims.Str("Background:"), Prims.Eol));
+		private static void CreateFeatureParsers()
+		{
+			var backgroundHeaderParser = Ops.Seq(WhitespaceParser, Ops.Seq(Ops.Seq(Prims.Str("Background:"), WhitespaceParser), Prims.Eol));
 			var featureHeaderParser = Ops.Seq(WhitespaceParser, Ops.Seq(Prims.Str("Feature:"), Prims.Rol));
 			var scenarioHeaderParser = Ops.Seq(WhitespaceParser, Ops.Seq(Prims.Str("Scenario:"), Prims.Rol));
 			var scenarioOutlineHeaderParser = Ops.Seq(WhitespaceParser, Ops.Seq(Prims.Str("Scenario Outline:"), Prims.Rol));
@@ -41,7 +54,6 @@ namespace Nucumber.Core.Parsers {
 			var backgroundParser = Ops.Seq(backgroundHeaderParser, Ops.Klenee(StepParser));
 
 			var featureParser = Ops.Seq(featureHeaderParser, Ops.Klenee(Prims.Rol));
-
 		}
 
 		#endregion
