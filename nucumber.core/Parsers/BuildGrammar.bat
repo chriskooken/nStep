8f740ea7387137db9d533816dcb1e9df53bda745
adamrmoss <adamrmoss@yahoo.com>
2009-09-22 16:13:19 -0400

    First stable version of grammar.

--- nucumber.core/Parsers/BuildGrammar.bat
+++ nucumber.core/Parsers/BuildGrammar.bat
@@ -1,1 +1,1 @@
-java -jar ..\..\Lib\grammatica-1.5.jar gherkin.grammar --csoutput . --csnamespace Nucumber.Core.Parsers
+java -jar ..\..\Lib\grammatica-1.5.jar gherkin.grammar --csoutput .\Generated --csnamespace Nucumber.Core.Parsers.Generated
