d6e1256efa57752f8cbc90f3ebb47a5b2b543ee3
Chris Kooken <chris.kooken@gmail.com>
2009-09-23 19:14:12 -0400

    cleaned up

--- Nucumber/Program.cs
+++ Nucumber/Program.cs
@@ -17,22 +17,22 @@ namespace Nucumber.App
 
         static void Main(string[] args)
         {
-            # if DEBUG
+# if DEBUG
             args = new[]
                        {
                            Path.GetFullPath(@"..\..\..\example\bin\Debug\example.dll"),
                            Path.GetFullPath(@"..\..\..\example\example.feature")
                        };
-            # endif
+# endif
             new Program().Run(args);
         }
 
         private void Run(string[] args)
         {
-            
+
             AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
-            
-            
+
+
             formatter = new ConsoleOutputFormatter("Nucumber", new CSharpSyntaxSuggester());
 
             var assemblyFile = new FileInfo(args.FirstOrDefault());
@@ -40,7 +40,7 @@ namespace Nucumber.App
             var worldViews = new WorldViewDictionary();
             worldViews.Import(AssemblyLoader.GetWorldViewProviders(assemblyFile));
 
-            EnvironmentBase env = AssemblyLoader.LoadEnvironment(new []{assemblyFile});
+            EnvironmentBase env = AssemblyLoader.LoadEnvironment(new[] { assemblyFile });
 
             env.GlobalBegin(worldViews);
 
@@ -51,31 +51,20 @@ namespace Nucumber.App
             feature.Parse(args[1]);
 
             new FeatureExecutor(formatter, StepMother).ExecuteFeature(feature);
-           
+
             env.GlobalExit(worldViews);
             formatter.WriteResults(StepMother);
         }
 
         Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
-            //return Assembly.LoadFile(Path.GetFullPath(@"..\..\..\example\bin\debug\ThoughtWorks.Selenium.Core.dll"));
-            //This handler is called only when the common language runtime tries to bind to the assembly and fails.
+            var strTempAssmbPath =
+                Path.GetFullPath(@"..\..\..\example\bin\debug\" + args.Name.Substring(0, args.Name.IndexOf(",")) +
+                                 ".dll");
 
-        
-                    //Build the path of the assembly from where it has to be loaded.				
-                    var strTempAssmbPath = Path.GetFullPath(@"..\..\..\example\bin\debug\" + args.Name.Substring(0, args.Name.IndexOf(",")) + ".dll");
-                  
-                
-
-            
-            //Load the assembly from the specified path. 					
             var MyAssembly = Assembly.LoadFrom(strTempAssmbPath);
 
-            //Return the loaded assembly.
             return MyAssembly;
-
         }
-
-       
     }
 }
