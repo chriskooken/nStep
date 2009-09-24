f181963e61f510b6fee32d8a399595ab5d5621dd
Chris Kooken <chris.kooken@gmail.com>
2009-09-24 02:27:59 -0400

    added selenium extension methods

---
+++ example/Selenium/SeleniumExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Selenium;
+
+namespace Cucumber.Selenium
+{
+	static class SeleniumExtensions
+	{
+        public static void ClickByLinkText(this DefaultSelenium browser, string linkText)
+        {
+            browser.Click("link="+linkText);
+        }
+
+        public static void ClickButtonByText(this DefaultSelenium browser, string linkText)
+        {
+            browser.Click("//input[@value='" + linkText + "' and (@type='button' or @type='submit')]");
+        }  
+        
+        public static void ClickById(this DefaultSelenium browser, string id)
+        {
+            browser.Click("id="+id);
+        }
+
+        public static void ClickByName(this DefaultSelenium browser, string name)
+        {
+            browser.Click("name=" + name);
+        }
+
+        public static void ClickByCssLocator(this DefaultSelenium browser, string locator)
+        {
+            browser.Click("css=" + locator);
+        }
+
+        public static Uri GetUrl(this DefaultSelenium browser)
+        {
+            Uri uri = new Uri(browser.GetLocation());
+            return uri;
+        }
+	}
+}
