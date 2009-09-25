2c90086f1bd1d76959bb4c001ccd9e0cae776c59
Chris Kooken <chris.kooken@gmail.com>
2009-09-25 15:44:20 -0400

    fixed some broken tests

--- Nucumber/CommandLineUtilities/ISuggestSyntax.cs
+++
@@ -1,9 +0,0 @@
-using Nucumber.Core;
-
-namespace Nucumber.App.CommandLineUtilities
-{
-    public interface ISuggestSyntax
-    {
-        string TurnFeatureIntoSnippet(FeatureStep step);
-    }
-}
