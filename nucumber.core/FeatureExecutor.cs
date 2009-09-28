eeb80773cad27d34a7fa893478b2a8b42a4133f1
Brendan Erwin <brendanjerwin@gmail.com>
2009-09-28 16:45:47 -0400

    Scenario Hooks. Still need to load them from assemblies.

--- nucumber.core/FeatureExecutor.cs
+++ nucumber.core/FeatureExecutor.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Nucumber.Core.Features;
+using Nucumber.Framework;
+using Nucumber.Framework.ScenarioHooks;
 
 namespace Nucumber.Core
 {
@@ -13,14 +16,18 @@ namespace Nucumber.Core
     }
     public class FeatureExecutor
     {
-        private readonly IFormatOutput Console;
-        private readonly StepMother StepMother;
+        private readonly IFormatOutput console;
+        private readonly StepMother stepMother;
+        private readonly IEnumerable<BeforeScenarioHook> beforeScenarioHooks;
+        private readonly IEnumerable<AfterScenarioHook> afterScenarioHooks;
         bool backgroundDisplayedOnce;
 
-        public FeatureExecutor(IFormatOutput console, StepMother stepMother)
+        public FeatureExecutor(IFormatOutput console, StepMother stepMother, IEnumerable<BeforeScenarioHook> beforeScenarioHooks, IEnumerable<AfterScenarioHook> afterScenarioHooks)
         {
-            this.Console = console;
-            this.StepMother = stepMother;
+            this.console = console;
+            this.stepMother = stepMother;
+            this.beforeScenarioHooks = beforeScenarioHooks;
+            this.afterScenarioHooks = afterScenarioHooks;
         }
 
         public void ExecuteFeature(Feature feature, int lineNmber)
@@ -31,11 +38,11 @@ namespace Nucumber.Core
                     ExecuteFeature(feature);
                     break;
                 case FeatureParts.Background:
-                    Console.WriteFeatureHeading(feature);
+                    console.WriteFeatureHeading(feature);
                     ExecuteBackground(feature);
                     break;
                 case FeatureParts.Scenario:
-                    Console.WriteFeatureHeading(feature);
+                    console.WriteFeatureHeading(feature);
                     ExecuteScenario(feature.GetScenarioAt(lineNmber), feature);
                     break;
                 case FeatureParts.ScenarioOutlineExample:
@@ -46,7 +53,7 @@ namespace Nucumber.Core
 
         public void ExecuteFeature(Feature feature)
         {
-            Console.WriteFeatureHeading(feature);
+            console.WriteFeatureHeading(feature);
 
             foreach (var item in feature.Items)
             {
@@ -58,25 +65,60 @@ namespace Nucumber.Core
 
         private void ExecuteScenario(Scenario scenario, Feature feature)
         {
+            ExecuteBeforeScenarioHooks(scenario.Tags);
 			ExecuteBackground(feature);
             SkippingSteps = false;
-            Console.WriteScenarioTitle(scenario);
+            console.WriteScenarioTitle(scenario);
             foreach (var step in scenario.Steps)
             {
                 ExecuteStep(step);
             }
+            ExecuteAfterScenarioHooks(scenario.Tags, new ScenarioResult(null)); //TODO: Load an appropriate scenarioResult here...
+            console.WriteLineBreak();
+        }
 
-            Console.WriteLineBreak();
+        private void ExecuteAfterScenarioHooks(IEnumerable<string> tags, ScenarioResult scenarioResult)
+        {
+            foreach (var hook in afterScenarioHooks)
+            {
+                if (ShouldHookExecute(hook, tags))
+                    hook.Action.Invoke(scenarioResult);
+            }
+        }
+
+        private void ExecuteBeforeScenarioHooks(IEnumerable<string> tags)
+        {
+            foreach (var hook in beforeScenarioHooks)
+            {
+                if (ShouldHookExecute(hook, tags))
+                    hook.Action.Invoke();
+            }
+        }
+
+        private static bool ShouldHookExecute(ScenarioHook hook, IEnumerable<string> tags)
+        {
+            var shouldExecute = true;
+                
+            if (hook.Tags.Count()>1)
+            {
+                shouldExecute = false;
+                foreach (var tag in tags)
+                {
+                    if (!hook.Tags.Contains(tag)) continue;
+                    shouldExecute = true;
+                    break;
+                }
+            }
+            return shouldExecute;
         }
 
         private void ExecuteBackground(Feature feature)
         {
-			// TODO: Make sure this never gets executed unless background exists
 			if (feature.Background == null)
 				return;
 
             if (feature.Background.Steps.Count > 0)
-                Console.WriteBackgroundHeading(feature.Background);
+                console.WriteBackgroundHeading(feature.Background);
 
             SkippingSteps = false;
             foreach (var step in feature.Background.Steps)
@@ -84,35 +126,35 @@ namespace Nucumber.Core
                 ExecuteStep(step);
             }
 
-            Console.WriteLineBreak();
+            console.WriteLineBreak();
         }
 
         private bool SkippingSteps { get; set; }
 
         private void ExecuteStep(FeatureStep s)
         {
-            StepMother.ChekForMissingStep(s);
+            stepMother.ChekForMissingStep(s);
 
             if (SkippingSteps)
             {
-                Console.WriteSkippedFeatureLine(s);
+                console.WriteSkippedFeatureLine(s);
                 return;
             }
             SkippingSteps = true;
-            switch (StepMother.ProcessStep(s))
+            switch (stepMother.ProcessStep(s))
             {
                 case StepRunResults.Passed:
                     SkippingSteps = false;
-                    Console.WritePassedFeatureLine(s, StepMother.LastProcessStepDefinition);
+                    console.WritePassedFeatureLine(s, stepMother.LastProcessStepDefinition);
                     break;
                 case StepRunResults.Failed:
-                    Console.WriteException(s, StepMother.LastProcessStepException);
+                    console.WriteException(s, stepMother.LastProcessStepException);
                     break;
                 case StepRunResults.Pending:
-                    Console.WritePendingFeatureLine(s);
+                    console.WritePendingFeatureLine(s);
                     break;
                 case StepRunResults.Missing:
-                    Console.WritePendingFeatureLine(s);
+                    console.WritePendingFeatureLine(s);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
--- nucumber.core/Features/Scenario.cs
+++ nucumber.core/Features/Scenario.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 
 namespace Nucumber.Core.Features
 {
 	public class Scenario : FeatureItem
 	{
+	    public IEnumerable<string> Tags { get; set; }
 	}
 }
