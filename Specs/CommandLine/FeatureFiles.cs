b8eaaff360de9319adae6ab7ba957ec0c8983228
adamrmoss <adamrmoss@yahoo.com>
2009-10-18 14:08:54 -0400

    Move some Spec fixtures inside 'Spec' namespace.

--- Specs/CommandLine/FeatureFiles.cs
+++ Specs/CommandLine/FeatureFiles.cs
@@ -6,337 +6,320 @@ using nStep.App.CommandLineUtilities;
 using NUnit.Framework;
 using System.Collections.Generic;
 
-namespace CommandLine
+namespace Specs.CommandLine
 {
+	public abstract class ConsoleOptionsTestBase
+	{
+		protected TestOptions options;
+		protected string[] args;
 
-    public abstract class ConsoleOptionsTestBase
-    {
-        protected TestOptions options;
-        protected string[] args;
+		[TestFixtureSetUp]
+		public virtual void SetUp()
+		{
+			InitializeArguments();
 
-        [TestFixtureSetUp]
-        public virtual void SetUp()
-        {
-            InitializeArguments();
+			Assert.DoesNotThrow(() =>
+			                    options = new TestOptions().Parse<TestOptions>(args));
+		}
 
-            Assert.DoesNotThrow(() =>
-                                options = new TestOptions().Parse<TestOptions>(args));
-        }
+		protected abstract void InitializeArguments(); 
+	}
 
-        protected abstract void InitializeArguments(); 
-    }
+	[TestFixture]
+	public class If_Parse_Is_called_no_parameters : ConsoleOptionsTestBase
+	{
+		[TestFixtureSetUp]
+		public override void SetUp()
+		{
+			InitializeArguments();
+		}
 
-    [TestFixture]
-    public class If_Parse_Is_called_no_parameters : ConsoleOptionsTestBase
-    {
-        [TestFixtureSetUp]
-        public override void SetUp()
-        {
-            InitializeArguments();
-        }
+		[Test]
+		public void Then_It_Should_Error()
+		{
+			Assert.Throws(typeof (ConsoleOptionsException),
+			              () => options = new TestOptions().Parse<TestOptions>(args) );
+		}
 
-        [Test]
-        public void Then_It_Should_Error()
-        {
-            Assert.Throws(typeof (ConsoleOptionsException),
-                () => options = new TestOptions().Parse<TestOptions>(args) );
-        }
+		protected override void InitializeArguments()
+		{
+			args = new string[] { };
+		}
+	}
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] { };
-        }
-    }
+	[TestFixture]
+	public class If_Parse_Is_called_NULL_parameters : ConsoleOptionsTestBase
+	{
+		[TestFixtureSetUp]
+		public override void SetUp()
+		{
+			InitializeArguments();
+		}
 
-    [TestFixture]
-    public class If_Parse_Is_called_NULL_parameters : ConsoleOptionsTestBase
-    {
-        [TestFixtureSetUp]
-        public override void SetUp()
-        {
-            InitializeArguments();
-        }
+		[Test]
+		public void Then_It_Should_Error()
+		{
+			Assert.Throws(typeof(ConsoleOptionsException),
+			              () => options = new TestOptions().Parse<TestOptions>(args));
+		}
 
-        [Test]
-        public void Then_It_Should_Error()
-        {
-            Assert.Throws(typeof(ConsoleOptionsException),
-                () => options = new TestOptions().Parse<TestOptions>(args));
-        }
+		protected override void InitializeArguments()
+		{
+			args = null;
+		}
+	}
 
-        protected override void InitializeArguments()
-        {
-            args = null;
-        }
-    }
+	[TestFixture]
+	public class If_Parse_Is_called_with_one_parameter_and_no_flags_ : ConsoleOptionsTestBase
+	{
+		[Test]
+		public void Then_it_should_return_a_ConsoleOptionsBase_of_type_ConsoleOptions()
+		{
+			options.Should().Be.InstanceOf<TestOptions>();
+		}
 
+		[Test]
+		public void Then_the_required_attribute_should_be_set()
+		{
+			options.FeatureFiles.Should().Be.EqualTo(args[0]);
+		}
 
-    [TestFixture]
-    public class If_Parse_Is_called_with_one_parameter_and_no_flags_ : ConsoleOptionsTestBase
-    {
-        [Test]
-        public void Then_it_should_return_a_ConsoleOptionsBase_of_type_ConsoleOptions()
-        {
-            options.Should().Be.InstanceOf<TestOptions>();
-        }
+		protected override void InitializeArguments()
+		{
+			args = new string[] { "C:/Projects/nStep/example" };
+		}
+	}
 
-        [Test]
-        public void Then_the_required_attribute_should_be_set()
-        {
-            options.FeatureFiles.Should().Be.EqualTo(args[0]);
-        }
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_verbose_flag_ : ConsoleOptionsTestBase
+	{
+		[Test]
+		public void Then_the_verbose_option_should_be_set()
+		{
+			options.Verbose.Should().Be.True();
+		}
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] { "C:/Projects/nStep/example" };
-        }
-    }
+		protected override void InitializeArguments()
+		{
+			args = new string[] { "C:/Projects/nStep/example", "-v" };
+		}
+	}
 
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_verbose_flag_ : ConsoleOptionsTestBase
-    {
-        [Test]
-        public void Then_the_verbose_option_should_be_set()
-        {
-            options.Verbose.Should().Be.True();
-        }
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_verbose_option : ConsoleOptionsTestBase
+	{
+		[Test]
+		public void Then_the_verbose_option_should_be_set()
+		{
+			options.Verbose.Should().Be.True();
+		}
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] { "C:/Projects/nStep/example", "-v" };
-        }
-    }
+		protected override void InitializeArguments()
+		{
+			args = new string[] { "C:/Projects/nStep/example", "-verbose" };
+		}
+	}
 
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_verbose_option : ConsoleOptionsTestBase
-    {
-        [Test]
-        public void Then_the_verbose_option_should_be_set()
-        {
-            options.Verbose.Should().Be.True();
-        }
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_require_flag_and_no_parameters : ConsoleOptionsTestBase
+	{
+		[TestFixtureSetUp]
+		public override void SetUp()
+		{
+			InitializeArguments();
+		}
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] { "C:/Projects/nStep/example", "-verbose" };
-        }
-    }
+		[Test]
+		public void Then_it_should_error()
+		{
+			Assert.Throws(
+				typeof(ConsoleOptionsException),
+				() => options = new TestOptions().Parse<TestOptions>(args)
+				);
+		}
 
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_require_flag_and_no_parameters : ConsoleOptionsTestBase
-    {
-        [TestFixtureSetUp]
-        public override void SetUp()
-        {
-            InitializeArguments();
-        }
+		protected override void InitializeArguments()
+		{
+			args = new string[] {"C:/Projects/nStep/example", "-r" };
+		}
+	}
 
-        [Test]
-        public void Then_it_should_error()
-        {
-            Assert.Throws(
-                        typeof(ConsoleOptionsException),
-                        () => options = new TestOptions().Parse<TestOptions>(args)
-                        );
-        }
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_require_option_and_no_parameters : ConsoleOptionsTestBase
+	{
+		[TestFixtureSetUp]
+		public override void SetUp()
+		{
+			InitializeArguments();
+		}
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] {"C:/Projects/nStep/example", "-r" };
-        }
-    }
+		[Test]
+		public void Then_it_should_error()
+		{
+			Assert.Throws(
+				typeof(ConsoleOptionsException),
+				() => options = new TestOptions().Parse<TestOptions>(args)
+				);
+		}
 
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_require_option_and_no_parameters : ConsoleOptionsTestBase
-    {
-        [TestFixtureSetUp]
-        public override void SetUp()
-        {
-            InitializeArguments();
-        }
+		protected override void InitializeArguments()
+		{
+			args = new string[] {"C:/Projects/nStep/example", "-require" };
+		}
+	}
 
-        [Test]
-        public void Then_it_should_error()
-        {
-            Assert.Throws(
-                        typeof(ConsoleOptionsException),
-                        () => options = new TestOptions().Parse<TestOptions>(args)
-                        );
-        }
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_require_flag_and_one_parameter : ConsoleOptionsTestBase
+	{
+		private string someAssembly = "SomeAssembly";
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] {"C:/Projects/nStep/example", "-require" };
-        }
-    }
+		[Test]
+		public void Then_the_assembly_option_should_be_set()
+		{
+			options.Assemblies.Count.Should().Be.EqualTo(1);
+			options.Assemblies.First().Should().Be.EqualTo(someAssembly);
+		}
 
+		protected override void InitializeArguments()
+		{
+			args = new string[] { "C:/Projects/nStep/example", "-r", someAssembly };
+		}
+	}
 
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_require_flag_and_one_parameter : ConsoleOptionsTestBase
-    {
-        private string someAssembly = "SomeAssembly";
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_require_option_and_one_parameter : ConsoleOptionsTestBase
+	{
+		private string someAssembly = "SomeAssembly";
 
-        [Test]
-        public void Then_the_assembly_option_should_be_set()
-        {
-            options.Assemblies.Count.Should().Be.EqualTo(1);
-            options.Assemblies.First().Should().Be.EqualTo(someAssembly);
-        }
+		[Test]
+		public void Then_the_assembly_option_should_be_set()
+		{
+			options.Assemblies.Count.Should().Be.EqualTo(1);
+			options.Assemblies.First().Should().Be.EqualTo(someAssembly);
+		}
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] { "C:/Projects/nStep/example", "-r", someAssembly };
-        }
-    }
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_require_option_and_one_parameter : ConsoleOptionsTestBase
-    {
-        private string someAssembly = "SomeAssembly";
+		protected override void InitializeArguments()
+		{
+			args = new string[] { "C:/Projects/nStep/example", "-require", someAssembly };
+		}
+	}
 
-        [Test]
-        public void Then_the_assembly_option_should_be_set()
-        {
-            options.Assemblies.Count.Should().Be.EqualTo(1);
-            options.Assemblies.First().Should().Be.EqualTo(someAssembly);
-        }
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_require_flag_and_more_than_one_parameter : ConsoleOptionsTestBase
+	{
+		private IList<string> someAssembly = new List<string>(){"SomeAssembly","someotherassembly"};
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] { "C:/Projects/nStep/example", "-require", someAssembly };
-        }
-    }
+		[Test]
+		public void Then_the_assembly_option_should_be_set()
+		{
+			options.Assemblies.SequenceEqual(someAssembly).Should().Be.True();
+		}
 
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_require_flag_and_more_than_one_parameter : ConsoleOptionsTestBase
-    {
-        private IList<string> someAssembly = new List<string>(){"SomeAssembly","someotherassembly"};
+		protected override void InitializeArguments()
+		{
+			args = new string[] { "C:/Projects/nStep/example", "-r", someAssembly[0], someAssembly[1] };
+		}
+	}
 
-        [Test]
-        public void Then_the_assembly_option_should_be_set()
-        {
-            options.Assemblies.SequenceEqual(someAssembly).Should().Be.True();
-        }
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_require_option_and_more_than_one_parameter : ConsoleOptionsTestBase
+	{
+		private IList<string> someAssembly = new List<string>(){"SomeAssembly","someotherassembly"};
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] { "C:/Projects/nStep/example", "-r", someAssembly[0], someAssembly[1] };
-        }
-    }
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_require_option_and_more_than_one_parameter : ConsoleOptionsTestBase
-    {
-        private IList<string> someAssembly = new List<string>(){"SomeAssembly","someotherassembly"};
+		[Test]
+		public void Then_the_assembly_option_should_be_set()
+		{
+			options.Assemblies.SequenceEqual(someAssembly).Should().Be.True();
+		}
 
-        [Test]
-        public void Then_the_assembly_option_should_be_set()
-        {
-            options.Assemblies.SequenceEqual(someAssembly).Should().Be.True();
-        }
+		protected override void InitializeArguments()
+		{
+			args = new string[] {"C:/Projects/nStep/example", "-require", someAssembly[0],someAssembly[1]};
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] {"C:/Projects/nStep/example", "-require", someAssembly[0],someAssembly[1]};
+		}
+	}
 
-        }
-    }
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_format_flag_and_no_parameters : ConsoleOptionsTestBase
-    {
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_format_flag_and_no_parameters : ConsoleOptionsTestBase
+	{
 
-        [TestFixtureSetUp]
-        public override void SetUp()
-        {
-            InitializeArguments();
-        }
+		[TestFixtureSetUp]
+		public override void SetUp()
+		{
+			InitializeArguments();
+		}
 
-        [Test]
-        public void Then_it_should_error()
-        {
-            Assert.Throws(
-                        typeof(ConsoleOptionsException),
-                        () => options = new TestOptions().Parse<TestOptions>(args)
-                        );
-        }
+		[Test]
+		public void Then_it_should_error()
+		{
+			Assert.Throws(
+				typeof(ConsoleOptionsException),
+				() => options = new TestOptions().Parse<TestOptions>(args)
+				);
+		}
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] {"C:/Projects/nStep/example", "-f" };
-        }
-    }
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_format_option_and_no_parameters : ConsoleOptionsTestBase
-    {
+		protected override void InitializeArguments()
+		{
+			args = new string[] {"C:/Projects/nStep/example", "-f" };
+		}
+	}
 
-        [TestFixtureSetUp]
-        public override void SetUp()
-        {
-            InitializeArguments();
-        }
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_format_option_and_no_parameters : ConsoleOptionsTestBase
+	{
 
-        [Test]
-        public void Then_it_should_error()
-        {
-            Assert.Throws(
-                        typeof(ConsoleOptionsException),
-                        () => options = new TestOptions().Parse<TestOptions>(args)
-                        );
-        }
+		[TestFixtureSetUp]
+		public override void SetUp()
+		{
+			InitializeArguments();
+		}
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] { "C:/Projects/nStep/example", "-format" };
-        }
-    }
+		[Test]
+		public void Then_it_should_error()
+		{
+			Assert.Throws(
+				typeof(ConsoleOptionsException),
+				() => options = new TestOptions().Parse<TestOptions>(args)
+				);
+		}
 
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_format_flag_and_one_parameter : ConsoleOptionsTestBase
-    {
-        private Format format = Format.Html;
+		protected override void InitializeArguments()
+		{
+			args = new string[] { "C:/Projects/nStep/example", "-format" };
+		}
+	}
 
-        [Test]
-        public void Then_the_format_option_should_be_set()
-        {
-            options.Format.Should().Be.EqualTo(format);
-        }
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_format_flag_and_one_parameter : ConsoleOptionsTestBase
+	{
+		private Format format = Format.Html;
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] { "C:/Projects/nStep/example", "-f", format.ToString() };
-        }
-    }
+		[Test]
+		public void Then_the_format_option_should_be_set()
+		{
+			options.Format.Should().Be.EqualTo(format);
+		}
 
-    [TestFixture]
-    public class If_Parse_Is_called_with_the_format_option_and_one_parameter : ConsoleOptionsTestBase
-    {
-        private Format format = Format.Html;
+		protected override void InitializeArguments()
+		{
+			args = new string[] { "C:/Projects/nStep/example", "-f", format.ToString() };
+		}
+	}
 
-        [Test]
-        public void Then_the_format_option_should_be_set()
-        {
-            options.Format.Should().Be.EqualTo(format);
-        }
+	[TestFixture]
+	public class If_Parse_Is_called_with_the_format_option_and_one_parameter : ConsoleOptionsTestBase
+	{
+		private Format format = Format.Html;
 
-        protected override void InitializeArguments()
-        {
-            args = new string[] { "C:/Projects/nStep/example", "-format", format.ToString() };
-        }
-    }
+		[Test]
+		public void Then_the_format_option_should_be_set()
+		{
+			options.Format.Should().Be.EqualTo(format);
+		}
 
-    //[TestFixture]
-    //public class If_Parse_Is_called_with_the_lformat_option_and_one_parameter : ConsoleOptionsTestBase
-    //{
-    //    private List<Format> lformat = new List<Format>(){Format.Html,Format.Xml};
-
-    //    [Test]
-    //    public void Then_the_lformat_option_should_be_set()
-    //    {
-    //        options.LFormats.SequenceEqual(lformat).Should().Be.True();
-    //    }
-
-    //    protected override void InitializeArguments()
-    //    {
-    //        args = new string[] { "C:/Projects/nStep/example", "-lformat", lformat[0].ToString(), lformat[1].ToString() };
-    //    }
-    //}
-
+		protected override void InitializeArguments()
+		{
+			args = new string[] { "C:/Projects/nStep/example", "-format", format.ToString() };
+		}
+	}
 }
--- Specs/StepSetBase/Given.cs
+++ Specs/StepSetBase/Given.cs
@@ -3,18 +3,17 @@ using nStep.Framework.StepDefinitions;
 using NUnit.Framework;
 using System.Linq;
 
-namespace StepSetBase
+namespace Specs.StepSetBase
 {
-    [TestFixture]
-    public class GivenHandling : StepSetBase<string>
-    {
-        [Test]
-        public void it_should_register_a_Given_Step_as_a_Given()
-        {
-            Given("something", () => { return; });
-            StepDefinitions.Givens.First().Kind.Should().Be.EqualTo(StepKinds.Given);
-        }
+	[TestFixture]
+	public class GivenHandling : StepSetBase<string>
+	{
+		[Test]
+		public void it_should_register_a_Given_Step_as_a_Given()
+		{
+			Given("something", () => { return; });
+			StepDefinitions.Givens.First().Kind.Should().Be.EqualTo(StepKinds.Given);
+		}
 
-    }
-
+	}
 }
